8f0a719b223a59f816423ab5de5b36672ce24f7f
Kim HeeCheol <audwkd0618@gmail.com>
2024-05-24 02:52:39 +0900

    20240523 2차 커밋

---
+++ 04_servletjsp/ecl/jspPro/src/main/java/com/multi/shop/board/controller/BoardOneServlet.java
@@ -0,0 +1,71 @@
+package com.multi.shop.board.controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.multi.shop.board.model.dto.BoardDTO;
+import com.multi.shop.board.service.BoardService;
+import com.multi.shop.board.service.BoardServiceImpl;
+
+/**
+ * Servlet implementation class BoardListServlet
+ */
+@WebServlet(asyncSupported = true, urlPatterns = { "/board/selectone" })
+public class BoardOneServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private BoardService boardService = new BoardServiceImpl();
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public BoardOneServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String noStr = request.getParameter("no");
+		
+		int no = Integer.parseInt(noStr);
+		
+		String path = "";
+		
+		try {
+			
+			BoardDTO board = boardService.selectBoard(no);
+			
+			request.setAttribute("b", board);
+			
+			System.out.println(board);
+			
+			path = "/WEB-INF/views/board/board_detail.jsp";
+			
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			path = "/WEB-INF/views/common/errorPage.jsp";
+			request.setAttribute("msg", "게시물 상세 조회 실패!");
+		}
+		request.getRequestDispatcher(path).forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		// doGet(request, response);
+	}
+
+}
